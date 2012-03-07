aec6bb402182c47f7a168a019a11275dcbf38a0d
sylvai_n <sylvain.noelie@gmail.com>
2012-03-07 18:52:08 +0100

    interface graphique. pony !

---
+++ MyRainbowPlayer/TestUserControl/Trigger1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Interactivity;
+
+namespace TestUserControl
+{
+	public class Trigger1 : TriggerBase<DependencyObject>
+	{
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			// Insert code that you want to run when the Trigger is attached to an object.
+		}
+
+		protected override void OnDetaching()
+		{
+			base.OnDetaching();
+
+			// Insert code that you would want run when the Trigger is removed from an object.
+		}
+
+		//
+		// To invoke any associated Actions when this Trigger gets called, use
+		// this.InvokeActions(o) where o is an object that you can pass in as a parameter
+		//
+	}
+}
---
+++ MyRainbowPlayer/TestUserControl/ucMenuBar.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestUserControl
+{
+	/// <summary>
+	/// Interaction logic for ucMenuBar.xaml
+	/// </summary>
+	public partial class ucMenuBar : UserControl
+	{
+		public ucMenuBar()
+		{
+			this.InitializeComponent();
+		}
+
+		private void CreateaPlaylist(object sender, System.Windows.RoutedEventArgs e)
+		{
+			//Todo, jai fait un code vite fait mais faut voir si playlist
+			//existe pas deja etc
+			    Window window = new Window 
+   			    {
+                Title = "Create a New Playlist",
+                Content = new ucNewPlaylist(),
+				Height = 200,
+				Width = 435
+                // ResizeMode = NoResize defuck ?
+				};
+				var bc = new BrushConverter();
+				window.Background = (Brush)bc.ConvertFrom("#FF9EDBF8");
+                window.ShowDialog();
+		}
+
+		private void OpenNewFile(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+
+		private void PlayPauseClick(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+
+		private void StopButtonClick(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+
+		private void ShuffleButtonClick(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+
+		private void LoopButtonClick(object sender, System.Windows.RoutedEventArgs e)
+		{
+			// TODO: Add event handler implementation here.
+		}
+	}
+}
